dc79f9f291fb3d5ecfbfd1f928ef9ecbc69d70ef
abirajsa001 <abiraj_s@novalnetsolutions.com>
2026-01-21 11:47:48 +0530

    log updated

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -903,7 +903,7 @@ class PaymentService
     public function getProcessPaymentUrl()
     {
         $path = $this->webstoreHelper->getCurrentWebstoreConfiguration()->domainSsl . '/' . $this->sessionStorage->getLocaleSettings()->language . '/payment/novalnet/processPayment';	
-        $this->getLogger(__METHOD__)->error('Novalnet::getProcessPaymentUrl Post', $path);
+        $this->getLogger(__METHOD__)->error('Novalnet::getProcessPaymentUrl Postdata', $path);
         return $path;
     }
     
