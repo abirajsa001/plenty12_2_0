beffd4229291f0056fca29dea42a11c765581b62
abirajsa001 <abiraj_s@novalnetsolutions.com>
2026-02-06 12:36:05 +0530

    pluginApp

--- src/Assistants/NovalnetAssistant.php
+++ src/Assistants/NovalnetAssistant.php
@@ -947,20 +947,18 @@ class NovalnetAssistant extends WizardProvider
     /**
      * Load the active country values
      */
-    protected function getDefaultCountries(array $availableCountries = []): array
+    protected function getDefaultCountries($availableCountries = array())
     {
-        if (empty($availableCountries)) {
-            return [];
-        }
+
         /** @var CountryRepositoryContract $countryRepository */
         $countryRepository = pluginApp(CountryRepositoryContract::class);
-        $result = [];
-        foreach ($countryRepository->getActiveCountriesList() as $country) {
-            if (isset($availableCountries[$country->id])) {
-                $result[$country->id] = $country->isoCode2;
-            }
+        $activeCountries = $countryRepository->getActiveCountriesList();
+        /** @var Country $country */
+        foreach ($activeCountries as $country) {
+            $this->activeCountries[$country->id] = $country->isoCode2;
         }
-        return $result;
+        
+        return array_column(array_intersect_key($availableCountries, $this->activeCountries), 'value');
     }
 
     /**
