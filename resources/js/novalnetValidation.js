3893420b832c1ac280c4cd7eab0f28073eeb41af
abirajsa001 <abiraj_s@novalnetsolutions.com>
2026-02-09 10:59:27 +0530

    novalnetValidation

--- resources/js/novalnetValidation.js
+++ resources/js/novalnetValidation.js
@@ -9,6 +9,26 @@ jQuery(document).ready(function() {
         // Update the input field with the numeric value
         jQuery(this).val(numericValue);
     });
+
+    function isValidAccountHolder(event) {
+        var charCode = (event.which) ? event.which : event.keyCode;
+        if ( !( charCode >= 65 && charCode <= 90 ) && // A-Z
+            !( charCode >= 97 && charCode <= 122 ) && // a-z
+            charCode !== 38 && // &
+            charCode !== 45 && // -
+            charCode !== 46 && // .
+            charCode !== 32 // space
+        ) {
+            event.preventDefault();
+            return false;
+        }
+        return true;
+    }
+
+    // Attach events
+    jQuery('#nn_account_holder').on('keypress keyup change', function (event) {
+        return isValidAccountHolder(event);
+    });
 });
 
 
