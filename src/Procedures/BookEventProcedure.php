f9105b0c5dbdddae4d8b4352d7064eebb0258a52
abirajsa001 <abiraj_s@novalnetsolutions.com>
2026-02-19 19:08:16 +0530

    booking details

--- src/Procedures/BookEventProcedure.php
+++ src/Procedures/BookEventProcedure.php
@@ -16,6 +16,8 @@ use Plenty\Modules\EventProcedures\Events\EventProceduresTriggered;
 use Plenty\Modules\Order\Models\Order;
 use Plenty\Modules\Order\Models\OrderType;
 use Plenty\Modules\Payment\Contracts\PaymentRepositoryContract;
+use Plenty\Modules\Basket\Models\Basket;
+use Plenty\Modules\Frontend\Services\AccountService;
 use Plenty\Plugin\Log\Loggable;
 
 /**
@@ -50,22 +52,31 @@ class BookEventProcedure
     private $paymentService;
 
     /**
+     *
+     * @var Basket
+     */
+    private $basket;
+
+    /**
      * Constructor.
      *
      * @param PaymentRepositoryContract $paymentRepository
      * @param PaymentHelper $paymentHelper
      * @param SettingsService $settingsService
      * @param PaymentService $paymentService
+     * @param Basket $basket
      */
     public function __construct(PaymentRepositoryContract $paymentRepository,
                                 PaymentHelper $paymentHelper,
                                 SettingsService $settingsService,
-                                PaymentService $paymentService)
+                                PaymentService $paymentService,
+                                Basket $basket)
     {
         $this->paymentRepository = $paymentRepository;
         $this->paymentHelper     = $paymentHelper;
         $this->settingsService   = $settingsService;
         $this->paymentService    = $paymentService;
+        $this->basket            = $basket;
     }
 
     /**
@@ -94,7 +105,7 @@ class BookEventProcedure
             // Get the proper order amount even the system currency and payment currency are differ
             if(count($order->amounts) > 1) {
                 foreach($order->amounts as $amount) {
-                    if($paymentCurrency == $amount->currency) {
+                    if($paymentCurrency == $amount->currency) { 
                        $refundAmount = (float) $amount->invoiceTotal; // Get the refunding amount
                     }
                 }
@@ -111,49 +122,123 @@ class BookEventProcedure
             $transactionDetails = $this->paymentService->getDetailsFromPaymentProperty($parentOrderId);
             $this->getLogger(__METHOD__)->error('Booking Event Procedure', [
                 'bookTransactionDetails' => $transactionDetails,
-                'paymentDetails' => $paymentDetails
+                'paymentDetails' => $paymentDetails,
+                'order' => $order,
+                'invoiceTotal' => (float) $order->amounts[0]->invoiceTotal
             ]);
-            if(in_array($transactionDetails['tx_status'], ['PENDING', 'CONFIRMED'])) {
-                // Novalnet access key
-                $privateKey = $this->settingsService->getPaymentSettingsValue('novalnet_private_key');
-                $paymentRequestData = [];
-                $paymentRequestData['transaction']['tid']    = $transactionDetails['tid'];
-                $paymentRequestData['transaction']['amount'] = (float) $refundAmount * 100;
-                $paymentRequestData['custom']['lang'] = strtoupper($orderLanguage);
-                $paymentRequestData['custom']['shop_invoked'] = 1;
-                // Send the payment capture/void call to Novalnet server
-                $paymentResponseData = $this->paymentHelper->executeCurl($paymentRequestData, NovalnetConstants::PAYMENT_URL, $privateKey);
-                $paymentResponseData = array_merge($paymentRequestData, $paymentResponseData);
-                // If refund is successful
-                if($paymentResponseData['result']['status'] == 'SUCCESS' && in_array($paymentResponseData['transaction']['status'], ['PENDING', 'CONFIRMED', 'DEACTIVATED'])) {
-                    // Booking text
-                    if(!empty($paymentResponseData['transaction']['refund']['tid'])) {
-                        $paymentResponseData['bookingText'] = sprintf($this->paymentHelper->getTranslatedText('refund_message_new_tid', $orderLanguage), $paymentResponseData['transaction']['tid'], str_replace('.', ',', sprintf('%0.2f', ($paymentResponseData['transaction']['refund']['amount'] / 100))) , $paymentCurrency, $paymentResponseData['transaction']['refund']['tid']);
-                    } else {
-                        $paymentResponseData['bookingText'] = sprintf($this->paymentHelper->getTranslatedText('refund_message', $orderLanguage), $paymentResponseData['transaction']['tid'], str_replace('.', ',', sprintf('%0.2f', ($paymentResponseData['transaction']['refund']['amount'] / 100))), $paymentCurrency, uniqid());
-                    }
-                    // Insert the refund details into Novalnet DB
-                    $this->paymentService->insertPaymentResponse($paymentResponseData);
-                    // Get refund status it is happened for Full amount or Partially
-                    $refundStatus = $this->paymentService->getRefundStatus($paymentResponseData['transaction']['order_no'], $paymentResponseData['transaction']['amount'], $paymentResponseData['transaction']['refund']['amount']);
-                    // Set the refund status it Partial or Full refund
-                    $paymentResponseData['refund'] = $refundStatus;
-                    if($order->typeId == OrderType::TYPE_CREDIT_NOTE) { // Create refund entry in credit note order
-                        $paymentResponseData['childOrderId'] = $childOrderId;
-                        $this->paymentHelper->createRefundPayment($paymentDetails, $paymentResponseData, $paymentResponseData['bookingText']);
-                    } else {
-                        // Get the Novalnet payment methods Id
-                        $mop = $this->paymentHelper->getPaymentMethodByKey(strtoupper($transactionDetails['paymentName']));
-                        $paymentResponseData['mop'] = $mop[0];
-                        // Create the payment to the plenty order
-                        $this->paymentHelper->createPlentyPayment($paymentResponseData);
-                    }
-                } else {
-                    $this->getLogger(__METHOD__)->error('Novalnet::Refund failed ' . $paymentResponseData['result']['status_text'], $e);
-                }
-            } else {
-                $this->getLogger(__METHOD__)->error('Novalnet::Refund Failed ' . $transactionDetails['order_no'], 'Transaction status is not valid');
-            }
+
+     // Get the customer billing and shipping details
+     $billingAddressId = $this->basket->customerInvoiceAddressId;
+     $shippingAddressId = $this->basket->customerShippingAddressId;
+     // Get the billing and shipping address Id from session during the reinititiate payment process
+     if(empty($billingAddressId)) {
+         $billingAddressId = $this->sessionStorage->getPlugin()->getValue('nnBillingAddressId');
+     }
+     if(empty($shippingAddressId)) {
+         $shippingAddressId = $this->sessionStorage->getPlugin()->getValue('nnShippingAddressId');
+     }
+     $billingAddress = $this->paymentHelper->getCustomerAddress((int) $billingAddressId);
+     $shippingAddress = $billingAddress;
+     if(!empty($shippingAddressId)) {
+         $shippingAddress = $this->paymentHelper->getCustomerAddress((int) $shippingAddressId);
+     }
+     // Get the customer name if the salutation as Person
+     $customerName = $this->paymentService->getCustomerName($billingAddress);
+
+     // Get the customerId
+     $account = pluginApp(AccountService::class);
+     $customerId = $account->getAccountContactId();
+
+     // Get the testMode value
+     $paymentKeyLower = strtolower((string) $paymentKey);
+
+     /** @var \Plenty\Modules\Frontend\Services\VatService $vatService */
+     $vatService = pluginApp(\Plenty\Modules\Frontend\Services\VatService::class);
+
+     //we have to manipulate the basket because its stupid and doesnt know if its netto or gross
+     if(!count($vatService->getCurrentTotalVats())) {
+         $basket->itemSum = $basket->itemSumNet;
+         $basket->shippingAmount = $basket->shippingAmountNet;
+         $basket->basketAmount = $basket->basketAmountNet;
+     }
+
+        // Build the Payment Request Parameters
+        $paymentRequestData = [];
+        // Building the merchant Data
+        $paymentRequestData['merchant'] = [
+            'signature'    => $this->settingsService->getPaymentSettingsValue('novalnet_public_key'),
+            'tariff'       => $this->settingsService->getPaymentSettingsValue('novalnet_tariff_id')
+        ];
+        // Building the customer Data
+        $paymentRequestData['customer'] = [
+            'first_name'   => !empty($billingAddress->firstName) ? $billingAddress->firstName : $customerName['firstName'],
+            'last_name'    => !empty($billingAddress->lastName) ? $billingAddress->lastName : $customerName['lastName'],
+            'gender'       => !empty($billingAddress->gender) ? $billingAddress->gender : 'u',
+            'email'        => $billingAddress->email,
+            'customer_no'  => !empty($customerId) ? $customerId : 'guest',
+            'customer_ip'  => $this->paymentHelper->getRemoteAddress()
+        ];
+        if(!empty($billingAddress->phone)) { // Check if phone field is given
+            $paymentRequestData['customer']['tel'] = $billingAddress->phone;
+        }
+        // Obtain the required billing and shipping details from the customer address object
+        $billingShippingDetails = $this->paymentHelper->getBillingShippingDetails($billingAddress, $shippingAddress);
+        $paymentRequestData['customer'] = array_merge($paymentRequestData['customer'], $billingShippingDetails);
+        // If the billing and shipping are equal, we notify it too
+        if($paymentRequestData['customer']['billing'] == $paymentRequestData['customer']['shipping']) {
+            $paymentRequestData['customer']['shipping']['same_as_billing'] = '1';
+        }
+        if(!empty($billingAddress->companyName) && ($this->settingsService->getPaymentSettingsValue('allow_b2b_customer', $paymentKeyLower) == true)) { // Check if company field is given in the shipping address
+            $paymentRequestData['customer']['billing']['company']  = $billingAddress->companyName;
+        }
+        if(!empty($billingAddress->state)) { // Check if state field is given in the billing address
+            $paymentRequestData['customer']['billing']['state']     = $billingAddress->state;
+        }
+        if(!empty($shippingAddress->companyName) && ($this->settingsService->getPaymentSettingsValue('allow_b2b_customer', $paymentKeyLower) == true)) { // Check if company field is given in the shipping address
+            $paymentRequestData['customer']['shipping']['company']  = $shippingAddress->companyName;
+        }
+        if(!empty($shippingAddress->state)) { // Check if state field is given in the shipping address
+            $paymentRequestData['customer']['shipping']['state']    = $shippingAddress->state;
+        }
+
+        // Unset the shipping details if the billing and shipping details are same
+        if(!empty($paymentRequestData['customer']['shipping']['same_as_billing'])) {
+            unset($paymentRequestData['customer']['shipping']);
+            $paymentRequestData['customer']['shipping']['same_as_billing'] = '1';
+        }
+        // Building the transaction Data
+        $paymentRequestData['transaction'] = [
+            'test_mode'         => ($this->settingsService->getPaymentSettingsValue('test_mode', $paymentKeyLower) == true) ? 1 : 0,
+            'amount'            => !empty($orderAmount) ? $orderAmount : $this->paymentHelper->convertAmountToSmallerUnit($basket->basketAmount),
+            'currency'          => !empty($this->sessionStorage->getPlugin()->getValue('orderCurrency')) ? $this->sessionStorage->getPlugin()->getValue('orderCurrency') : $basket->currency,
+            'system_name'       => 'Plentymarkets',
+            'system_version'    => NovalnetConstants::PLUGIN_VERSION,
+            'system_url'        => $this->webstoreHelper->getCurrentWebstoreConfiguration()->domainSsl,
+            'system_ip'         => $_SERVER['SERVER_ADDR']
+        ];
+
+
+        $this->getLogger(__METHOD__)->error('Booking Payment Request', [
+            'requestData' => $paymentRequestData,
+        ]);
+        $privateKey = $this->settingsService->getPaymentSettingsValue('novalnet_private_key');
+        $paymentResponseData = $this->paymentHelper->executeCurl($paymentRequestData, NovalnetConstants::PAYMENT_URL, $privateKey);
+
+        $this->getLogger(__METHOD__)->error('Booking Payment Response', [
+            'responseData' => $paymentResponseData,
+        ]);
+
+        $paymentResponseData['bookingText'] = sprintf($this->paymentHelper->getTranslatedText('refund_message_new_tid', $orderLanguage), $paymentResponseData['transaction']['tid'], str_replace('.', ',', sprintf('%0.2f', ($paymentResponseData['transaction']['refund']['amount'] / 100))) , $paymentCurrency, $paymentResponseData['transaction']['refund']['tid']);
+
+        // Insert the refund details into Novalnet DB
+        $this->paymentService->insertPaymentResponse($paymentResponseData);
+
+        // Get the Novalnet payment methods Id
+        $mop = $this->paymentHelper->getPaymentMethodByKey(strtoupper($transactionDetails['paymentName']));
+        $paymentResponseData['mop'] = $mop[0];
+        // Create the payment to the plenty order
+        $this->paymentHelper->createPlentyPayment($paymentResponseData);
+
         } catch(\Exception $e) {
             $this->getLogger(__METHOD__)->error('Novalnet::Refund failed ' . $transactionDetails['order_no'], $e);
         }
